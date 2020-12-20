6ae1e9dd4e060ddd2e23848b6cf34bda1ef99359
rohiaudio <rory@rohiaudio.com>
2020-12-19 19:06:12 -0500

    Init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Harmonic Interval Calculator
- Harmonic Interval Calculator
+The harmonic interval calculator is a tool you can use to find the intervals in Hz of a given fundamental.
+
+Written by: Rory (R.T.) Hinkel
+Website: https://www.rohiaudio.com
+
+Styling depends on bootstrap
+Calculations based off: http://www.sengpielaudio.com/calculator-harmonics.htm
---
+++ calculator.js
@@ -0,0 +1,9 @@
+function multiplyBy()
+{
+        fund = document.getElementById("fundamental").value;;
+        document.getElementById("3rd").innerHTML = 3 * fund;
+        document.getElementById("5th").innerHTML = 5 * fund;
+        document.getElementById("6th").innerHTML = 6 * fund;
+        document.getElementById("7th").innerHTML = 7 * fund;
+        document.getElementById("10th").innerHTML = 10 * fund;
+}
